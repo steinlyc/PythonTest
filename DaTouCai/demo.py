7c951653c6980747f3da545896629f7f9be7ebbf
steinlyc <331097543@qq.com>
2020-04-13 23:49:59 +0800

    大头菜脚本

---
+++ DaTouCai/demo.py
@@ -0,0 +1,54 @@
+import requests
+from twilio.rest import Client
+from threading import Timer
+import datetime
+
+
+def GetData():
+    res = requests.get('http://ttc.21hz.top/turniptrade/island/list/1')
+    res = res.json()
+    res = res['list']
+    return res
+
+
+def Sorted_list():
+    sorted_list = sorted(GetData(), key=lambda x: x['price'], reverse=True)
+    return sorted_list
+
+
+def GetPrice(t):
+    for item in Sorted_list():
+        if item['price'] > 500:
+            Send(item['price'])
+            t.cancel()
+            print('找到目标价格:' + str(item['price']))
+            return
+        else:
+            pass
+    print('查询中...' + GetLocalTime())
+
+
+def GetLocalTime():
+    local_time = datetime.datetime.now()
+    local_time = datetime.datetime.strftime(local_time, '%Y-%m-%d %H:%M:%S')
+    return str(local_time)
+
+
+def Send(price):
+    account_sid = "AC865ccf94cb13b997a3634654275ff8b2"
+    auth_token = "c06ecf26977476f968eff1218eec33ab"
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(to="+8615365108233",
+                                     from_="+18125419480",
+                                     body='找到大头菜目标价格:' + str(price))
+    print(message.sid)
+
+
+def PrinTime(time):
+    global t
+    t = Timer(time, PrinTime, (time, ))
+    GetPrice(t)
+    t.start()
+
+
+PrinTime(30)
